bfba3b48080547911c884abe4723ef8440d002d9
Robin Wright <robin@chalkcreation.co.uk>
2015-09-04 18:12:36 +0100

    config.php

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+
+//SQL connection details
+$SQLServer="localhost";
+$SQLUser="root";
+$SQLPass="root";
+$SQLDB="InkStock";
+
+//URL Setting
+$Location = "http://localhost:8888/Ink-Stock-Manager";
+
+//########################################################################################
+//###################### Nothing should be changed below this point ######################
+//########################################################################################
+
+//Attempting to connect to db
+$con=mysqli_connect($SQLServer,$SQLUser,$SQLPass,$SQLDB);
+//check if the connection was sucessful (Fingers Crossed)
+if (mysqli_connect_errno())
+{
+echo "Connection to the database fell over: " . mysqli_connect_error();
+}
+
+?>
